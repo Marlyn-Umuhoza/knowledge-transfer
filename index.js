87714f1364696207381f5a3d1e84050ccd53edd6
Marlyn Umuhoza <47836666+Marlyn-Umuhoza@users.noreply.github.com>
2021-11-11 16:44:29 +0200

    Update index.js
    Updated path to home page

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
-  res.sendFile('./home.html')
+  res.sendFile('.pages/home.html')
 })
 
 app.listen(port, () => {
